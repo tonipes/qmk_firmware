23b2744f3f24014980db62ab2923a970325d396d
Toni Pesola <pesola.toni@gmail.com>
2019-06-19 10:53:38 +0300

    Add custom keyboard layout

---
+++ keyboards/dz60/keymaps/tonipes/build_flash.sh
@@ -0,0 +1,9 @@
+# dfu-programmer atmega32u4 erase --force
+# dfu-programmer atmega32u4 flash /path/to/firmware.hex
+# dfu-programmer atmega32u4 reset
+
+# run this in the qmk_firmware directory
+make dz60:tonipes
+dfu-programmer atmega32u4 erase --force && \
+dfu-programmer atmega32u4 flash dz60_tonipes.hex && \
+dfu-programmer atmega32u4 reset
