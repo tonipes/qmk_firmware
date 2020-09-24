57941cc7e37c7b80f4757a67dd1fc7c2d0d7c576
Toni Pesola <pesola.toni@gmail.com>
2020-09-24 17:31:07 +0300

    update custom layout

--- keyboards/dz60/keymaps/tonipes/build_flash.sh
+++ keyboards/dz60/keymaps/tonipes/build_flash.sh
@@ -4,6 +4,17 @@
 
 # run this in the qmk_firmware directory
 make dz60:tonipes
+echo "Flashing in 5"
+sleep 1
+echo "Flashing in 4"
+sleep 1
+echo "Flashing in 3"
+sleep 1
+echo "Flashing in 2"
+sleep 1
+echo "Flashing in 1"
+sleep 1
+echo "Flashing NOW"
 dfu-programmer atmega32u4 erase --force && \
 dfu-programmer atmega32u4 flash dz60_tonipes.hex && \
 dfu-programmer atmega32u4 reset
